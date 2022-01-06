3ecea81b2ddf786a14e3c712e7fc3e1f1a8183f8
Gianelle Tianco <gianelle.tianco@gmail.com>
2022-01-05 22:13:39 -0500

    Made Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { useEffect, useState } from 'react'
 
 const Footer = (props) => {
-    return <div>footer</div>
+
+    //Thank you: https://www.digitalocean.com/community/tutorials/react-countdown-timer-react-hooks
+    
+    const calculateTimeLeft = () => {
+        let year = new Date().getFullYear();
+        const difference = +new Date(`${year}-10-1`) - +new Date();
+        let timeLeft = {};
+    
+        if (difference > 0) {
+          timeLeft = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          };
+        }
+    
+        return timeLeft;
+      };
+    
+      const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+      const [] = useState(new Date().getFullYear());
+    
+      useEffect(() => {
+        setTimeout(() => {
+          setTimeLeft(calculateTimeLeft());
+        }, 1000);
+      });
+    
+      const timerComponents = [];
+    
+      Object.keys(timeLeft).forEach((interval) => {
+        if (!timeLeft[interval]) {
+          return;
+        }
+    
+        timerComponents.push(
+          <span>
+            {timeLeft[interval]} {interval}{" "}
+          </span>
+        );
+      });
+    return <div>{timerComponents.length ? timerComponents : <span>Time's up!</span>}</div>
 };
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Header = (props) => {
-  return <div className="bg-header">
-            <h1>Mr &infin; Mrs</h1>
-        </div>;
+  return <a href="/"><div className="bg-header">
+          <h1>Mr &infin; Mrs</h1>
+        </div></a>;
 };
 
 export default Header;
